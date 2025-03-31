b45c1b622b54034556194c3ba5e197c1aa78865e
paulobarone <pmarquesbaronefilho18@protonmail.com>
2025-03-31 15:00:35 -0300

    use button component com Table

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,10 @@
+import Table from "./components/Table"
+
 function App() {
   return (
-    <h1 className="text-3xl">Hello World</h1>
+    <div className="h-screen p-2">
+      <Table />
+    </div>
   )
 }
 
