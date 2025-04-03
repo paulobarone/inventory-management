e945c70af228405af7582b0eb92d56bf3d2d71db
paulobarone <pmarquesbaronefilho18@protonmail.com>
2025-04-02 23:30:47 -0300

    add: Table MUI Component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,95 +1,10 @@
-import AddIcon from "@mui/icons-material/Add";
-import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
-import EditIcon from "@mui/icons-material/Edit";
-import FilterAltOutlinedIcon from "@mui/icons-material/FilterAltOutlined";
-import { Checkbox, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
-import { useState } from "react";
-import Button from "./components/Button";
-import { Product } from "./types/Product";
+import EnhancedTable from "./components/EnhancedTable";
 
 export default function App() {
-  const [products] = useState<Product[]>([
-    { id: 1, name: "Produto 1", price: 10, quantity: 5 },
-    { id: 2, name: "Produto 2", price: 20, quantity: 3 },
-    { id: 3, name: "Produto 3", price: 15, quantity: 8 },
-  ]);
-  const [selectedProducts, setSelectedProducts] = useState<number[]>([]);
-
-  const handleSelectedAll = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      setSelectedProducts(products.map((item) => item.id));
-    } else {
-      setSelectedProducts([]);
-    }
-  };
-
-  const handleSelected = (
-    event: React.ChangeEvent<HTMLInputElement>,
-    id: number
-  ) => {
-    if (event.target.checked) {
-      setSelectedProducts((prev) => [...prev, id]);
-    } else {
-      setSelectedProducts((prev) => prev.filter((item) => item !== id));
-    }
-  };
-
   return (
     <div className="h-screen p-2">
-      <div className="bg-white rounded-lg w-full h-full p-4">
-        <div className="flex justify-between items-center">
-          <h1 className="text-xl text-gray-800">Produtos</h1>
-          <div className="flex items-center gap-2">
-            <Button className="bg-rose-400"> <AddIcon /> Adicionar </Button>
-            <Button disabled={selectedProducts.length == 0}> <DeleteOutlineIcon /> Deletar </Button>
-            <Button> <FilterAltOutlinedIcon /> Filtros </Button>
-          </div>
-        </div>
-        <TableContainer>
-          <Table size="small">
-            <TableHead>
-              <TableRow>
-                <TableCell align="left">
-                  <Checkbox
-                    sx={{ "& .MuiSvgIcon-root": { fontSize: 24 } }}
-                    onChange={(event) => handleSelectedAll(event)}
-                    checked={selectedProducts.length === products.length && selectedProducts.length > 0}
-                  />
-                </TableCell>
-                <TableCell align="left">Nome</TableCell>
-                <TableCell align="left">Preço</TableCell>
-                <TableCell align="left">Quantidade</TableCell>
-                <TableCell align="right">Opções</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {products.map((product) => (
-                <TableRow
-                  key={product.id}
-                  className="hover:bg-gray-100 transition-colors duration-200"
-                >
-                  <TableCell align="left">
-                    <Checkbox
-                      sx={{ "& .MuiSvgIcon-root": { fontSize: 24 } }}
-                      onChange={(event) => handleSelected(event, product.id)}
-                      checked={selectedProducts.includes(product.id)}
-                    />
-                  </TableCell>
-                  <TableCell align="left">{product.name}</TableCell>
-                  <TableCell align="left">{`R$ ${product.price.toFixed(2)}`}</TableCell>
-                  <TableCell align="left">{product.quantity}</TableCell>
-                  <TableCell
-                    sx={{ display: "flex", flex: "row", gap: "8px" }}
-                    align="right"
-                  >
-                    <Button className="bg-rose-400"><DeleteOutlineIcon /></Button>
-                    <Button className="bg-gray-200"><EditIcon /></Button>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+      <div className="bg-gray-100 rounded-lg w-full h-full p-4 flex justify-center">
+        <EnhancedTable />
       </div>
     </div>
   );
---
+++ web/src/components/EnhancedTable.tsx
@@ -0,0 +1,382 @@
+import * as React from 'react';
+import { alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddIcon from '@mui/icons-material/Add';
+import { visuallyHidden } from '@mui/utils';
+import Button from './Button';
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import EditIcon from "@mui/icons-material/Edit";
+
+interface Data {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+  option: React.ReactNode;
+}
+
+function createData(
+  id: number,
+  name: string,
+  price: number,
+  quantity: number,
+  option: React.ReactNode = (
+    <>
+      <Button className="bg-rose-400"><DeleteOutlineIcon /></Button>
+      <Button className="bg-gray-200"><EditIcon /></Button>
+    </>
+  )
+): Data {
+  return {
+    id,
+    name,
+    price,
+    quantity,
+    option
+  };
+}
+
+const rows = [
+  createData(1, "Produto 1", 10, 5),
+  createData(2, "Produto 2", 15, 3),
+  createData(3, "Produto 3", 20, 5),
+  createData(4, "Produto 4", 35, 1),
+  createData(5, "Produto 5", 1230, 55),
+  createData(6, "Produto 6", 614, 5),
+  createData(7, "Produto 7", 10123, 23),
+  createData(8, "Produto 8", 10123, 52),
+  createData(9, "Produto 9", 1520, 55),
+  createData(10, "Produto 10", 1230.23, 4)
+];
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+type Order = 'asc' | 'desc';
+
+function getComparator<Key extends keyof Data>(
+  order: Order,
+  orderBy: Key,
+): (a: Data, b: Data) => number {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+interface HeadCell {
+  disablePadding: boolean;
+  id: keyof Data;
+  label?: string;
+  numeric: boolean;
+}
+
+const headCells: readonly HeadCell[] = [
+  {
+    id: 'name',
+    numeric: false,
+    disablePadding: true,
+    label: 'Produto',
+  },
+  {
+    id: 'price',
+    numeric: true,
+    disablePadding: false,
+    label: 'Preço',
+  },
+  {
+    id: 'quantity',
+    numeric: true,
+    disablePadding: false,
+    label: 'Quantidade',
+  },
+  {
+    id: 'option',
+    numeric: false,
+    disablePadding: false,
+  }
+];
+
+interface EnhancedTableProps {
+  numSelected: number;
+  onRequestSort: (event: React.MouseEvent<unknown>, property: keyof Data) => void;
+  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+  const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
+    props;
+  const createSortHandler =
+    (property: keyof Data) => (event: React.MouseEvent<unknown>) => {
+      onRequestSort(event, property);
+    };
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Checkbox
+            color="primary"
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+          />
+        </TableCell>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+            sortDirection={headCell.id !== 'option' && orderBy === headCell.id ? order : false}
+          >
+            {headCell.id !== 'option' ? (
+              <TableSortLabel
+                active={orderBy === headCell.id}
+                direction={orderBy === headCell.id ? order : 'asc'}
+                onClick={createSortHandler(headCell.id)}
+              >
+                {headCell.label}
+                {orderBy === headCell.id ? (
+                  <Box component="span" sx={visuallyHidden}>
+                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                  </Box>
+                ) : null}
+              </TableSortLabel>
+            ) : (
+              <TableSortLabel
+              >
+                {headCell.label}
+              </TableSortLabel>
+            )}
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+interface EnhancedTableToolbarProps {
+  numSelected: number;
+}
+function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
+  const { numSelected } = props;
+  return (
+    <Toolbar
+      sx={[
+        {
+          pl: { sm: 2 },
+          pr: { xs: 1, sm: 1 },
+        },
+        numSelected > 0 && {
+          bgcolor: (theme) =>
+            alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
+        },
+      ]}
+    >
+      {numSelected > 0 ? (
+        <Typography
+          sx={{ flex: '1 1 100%' }}
+          color="inherit"
+          variant="subtitle1"
+          component="div"
+        >
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography
+          sx={{ flex: '1 1 100%' }}
+          variant="h6"
+          id="tableTitle"
+          component="div"
+        >
+          Produtos
+        </Typography>
+      )}
+      {numSelected > 0 ? (
+        <Tooltip title="Deletar">
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Tooltip title="Adicionar Produto">
+          <IconButton>
+            <AddIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+    </Toolbar>
+  );
+}
+export default function EnhancedTable() {
+  const [order, setOrder] = React.useState<Order>('asc');
+  const [orderBy, setOrderBy] = React.useState<keyof Data>('name');
+  const [selected, setSelected] = React.useState<readonly number[]>([]);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleRequestSort = (
+    _event: React.MouseEvent<unknown>,
+    property: keyof Data,
+  ) => {
+    if (property === 'option') {
+      return;
+    }
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      const newSelected = rows.map((n) => n.id);
+      setSelected(newSelected);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (_event: React.MouseEvent<unknown>, id: number) => {
+    const selectedIndex = selected.indexOf(id);
+    let newSelected: readonly number[] = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (_event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const visibleRows = React.useMemo(
+    () =>
+      [...rows]
+        .sort(getComparator(order, orderBy))
+        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
+    [order, orderBy, page, rowsPerPage],
+  );
+
+  return (
+    <Box sx={{ width: '90%', maxWidth: '1500px' }}>
+      <Paper sx={{ width: '100%', mb: 2 }}>
+        <EnhancedTableToolbar numSelected={selected.length} />
+        <TableContainer>
+          <Table
+            sx={{ minWidth: 750 }}
+            aria-labelledby="tableTitle"
+          >
+            <EnhancedTableHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+            />
+            <TableBody>
+              {visibleRows.map((row, index) => {
+                const isItemSelected = selected.includes(row.id);
+                const labelId = `enhanced-table-checkbox-${index}`;
+
+                return (
+                  <TableRow
+                    aria-checked={isItemSelected}
+                    tabIndex={-1}
+                    key={row.id}
+                    selected={isItemSelected}
+                  >
+                    <TableCell padding="checkbox">
+                      <Checkbox
+                        color="primary"
+                        onClick={(event) => handleClick(event, row.id)}
+                        checked={isItemSelected}
+                      />
+                    </TableCell>
+                    <TableCell
+                      component="th"
+                      id={labelId}
+                      scope="row"
+                      padding="none"
+                    >
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">
+                      {row.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+                    </TableCell>
+                    <TableCell align="right">{row.quantity}</TableCell>
+                    <TableCell
+                      sx={{ display: "flex", flex: "row", gap: "8px" }}
+                      align="right"
+                    >
+                      {row.option}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: 53 * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </Box>
+  );
+}
