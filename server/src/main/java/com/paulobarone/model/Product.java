2e40f7610148ef53759236427ebe84481e0f1f85
paulobarone <pmarquesbaronefilho18@protonmail.com>
2025-04-23 14:34:33 -0300

    add Product model

---
+++ server/src/main/java/com/paulobarone/model/Product.java
@@ -0,0 +1,53 @@
+package com.paulobarone.model;
+
+public class Product {
+  private long id;
+  private String name;
+  private double price;
+  private int quantity;
+
+  public Product(long id, String name, double price, int quantity) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+  }
+
+  public Product(String name, double price, int quantity) {
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(int quantity) {
+    this.quantity = quantity;
+  }
+}
