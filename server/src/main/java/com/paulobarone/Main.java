40dc875d0c51c4cdd4f4a2c6da6af87ebba916b9
paulobarone <pmarquesbaronefilho18@protonmail.com>
2025-04-24 17:59:10 -0300

    fix connection with postgre db with .env

--- server/src/main/java/com/paulobarone/Main.java
+++ server/src/main/java/com/paulobarone/Main.java
@@ -1,7 +1,11 @@
 package com.paulobarone;
 
+import com.paulobarone.factory.ConnectionFactory;
+
+import java.sql.SQLException;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
+  public static void main(String[] args) throws SQLException {
+    ConnectionFactory.getConnection();
   }
 }
--- server/src/main/java/com/paulobarone/factory/ConnectionFactory.java
+++ server/src/main/java/com/paulobarone/factory/ConnectionFactory.java
@@ -1,13 +1,19 @@
 package com.paulobarone.factory;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-  private static final String dbUrl = System.getenv("DB_URL");
-
+  private static final String dbUrl;
   private static Connection currentConnection = null;
+
+  static {
+    Dotenv dotenv = Dotenv.load();
+    dbUrl = dotenv.get("DB_URL");
+  }
 
   public static Connection getConnection() throws SQLException {
     if(currentConnection == null || currentConnection.isClosed()) {
