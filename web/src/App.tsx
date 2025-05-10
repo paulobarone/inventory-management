d523950cb90b19a0365abc205dca16b81b50cb10
Paulo <pmarquesbaronefilho18@protonmail.com>
2025-05-10 12:26:55 -0300

    add ProductProvider

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,20 +1,11 @@
-import { useMemo, useState } from "react";
 import SimpleTable from "./components/SimpleTable";
 import DialogProvider from "./context/DialogProvider";
-import { ProductContext } from "./context/ProductContext";
-import { Product } from "./types/Product";
-import { products as productsData } from "./data/products";
 import TopHeader from "./components/TopHeader";
+import ProductProvider from "./context/ProductProvider";
 
 export default function App() {
-  const [products, setProducts] = useState<Product[] | []>(productsData);
-  const [selectedProducts, setSelectedProducts] = useState<number[]>([]);
-  const [selectAll, setSelectAll] = useState<boolean>(false);
-
-  const contextValue = useMemo(() => ({ products, setProducts, selectedProducts, setSelectedProducts, selectAll, setSelectAll }), [products, selectedProducts, selectAll]);
-
   return (
-    <ProductContext.Provider value={contextValue}>
+    <ProductProvider>
       <DialogProvider>
         <div className="flex items-center justify-center">
           <div className="bg-gray-100 rounded-lg w-full max-w-[1600px] h-screen p-2 flex gap-4 flex-col justify-center">
@@ -23,6 +14,6 @@ export default function App() {
           </div>
         </div>
       </DialogProvider>
-    </ProductContext.Provider>
+    </ProductProvider>
   );
 }
